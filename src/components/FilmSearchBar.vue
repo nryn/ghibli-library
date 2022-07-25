abad443ae73a0a5be5554fce25a527054cdb5da3
Narayan <narayanohanlon@gmail.com>
2022-07-26 01:25:17 +0900

    feat(search): add film search bar to film list pg
    it does not yet filter the film list, it's mostly just for show for now.

---
+++ src/components/FilmSearchBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const searchTerm = ref<string>('')
+const updateSearchTerm = () => {
+  console.log(`I would like to send ${searchTerm.value} to the store`)
+  // TODO send searchTerm.value to the store so it can be used to filter the film list...
+}
+</script>
+
+<template>
+  <div>
+    <input v-model="searchTerm" autofocus @input="updateSearchTerm" data-test="searchInput"/>
+  </div>
+</template>
--- src/pages/FilmsList.vue
+++ src/pages/FilmsList.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { store } from '../store'
 import FilmCard from '../components/FilmCard.vue';
+import FilmSearchBar from '../components/FilmSearchBar.vue';
 
 const films = store.films.slice(0, 10)
 </script>
 
 <template>
   <div>
+    <FilmSearchBar/>
     <FilmCard v-for="(film, index) in films" :key="index" :film="film"/>
   </div>
 </template>
