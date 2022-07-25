255c3df4c4c5c9190b9d827129d3d2f1452e4df0
Narayan <narayanohanlon@gmail.com>
2022-07-26 00:41:19 +0900

    test(overview): write e2e for main page

--- cypress/e2e/basic-features.cy.ts
+++ cypress/e2e/basic-features.cy.ts
@@ -4,4 +4,20 @@ describe('All the basic feature requirements', () => {
   it('can visit the app', () => {
     cy.visit('127.0.0.1:5173')
   })
+
+  it('shows a list of 10 films', () => {
+    cy.get('[data-test=FilmCard]').should('have.length', 10)
+  })
+  it('has the required information for all 10 visibile films', () => {
+    cy.get('[data-test=ClickableThumbnail]').should('have.length', 10)
+    cy.get('[data-test=Title]').should('have.length', 10)
+    cy.get('[data-test=ReleaseDate]').should('have.length', 10)
+    cy.get('[data-test=RunningTime]').should('have.length', 10)
+  })
+
+  // Test Ideas:
+  // "Be able to search for a film"
+  // "should see My neighbour totoro"
+  // "click through on the thumbnail to see more details"
+  // "see that Hayao Miyazaki is the director"
 })
