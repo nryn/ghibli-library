4b044e1d76d76b4d02817efd53ca2d9ffbf4bc7a
Narayan <narayanohanlon@gmail.com>
2022-07-26 17:55:32 +0900

    refactor(store): use vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,9 @@
+import { createStore } from 'vuex'
 import { stubbedFilmDataFromAPI } from "./stubs"
+import { State } from './types'
 
-export const store = {
-  films: stubbedFilmDataFromAPI
-}
+export const store = createStore<State>({
+  state: {
+    films: stubbedFilmDataFromAPI
+  }
+})
---
+++ src/store/types.d.ts
@@ -0,0 +1,24 @@
+export interface State {
+  films: Film[]
+}
+
+// Created manually after hitting the endpoint https://ghibliapi.herokuapp.com/films
+type Film = {
+  id: string
+  title: string
+  original_title: string
+  original_title_romanised: string
+  image: string
+  movie_banner: string
+  description: string
+  director: string
+  producer: string
+  release_date: string
+  running_time: string
+  rt_score: string
+  people: string[]
+  species: string[]
+  locations: string[]
+  vehicles: string[]
+  url: string
+}
