489956b5176b8b1893685ceccb9fa16633532b3f
Narayan <narayanohanlon@gmail.com>
2022-07-27 23:21:42 +0900

    feat(style): make the background look nice

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
 
 <template>
   <div class="font-mono">
+    <div
+    class="absolute inset-0 h-full bg-center bg-no-repeat bg-cover -z-10 blur-[120px] opacity-50"
+    :style="{ backgroundImage: 'url(/default-background.jpeg)' }"
+  ></div>
     <RouterView />
   </div>
 </template>
