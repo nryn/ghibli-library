ee891c1a062ecde515b79932e6af4f9b40d3448c
Narayan <narayanohanlon@gmail.com>
2022-07-27 22:51:05 +0900

    feat(nav): able to go back to list from detail pg

---
+++ src/components/BackToFilmsButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="fixed top-8 flex px-8 pb-8 w-full justify-center text-xl">
+      <router-link to="/"><p>⬆<span class="opacity-40 drop-shadow-2xl"> All Films</span></p></router-link>
+  </div>
+</template>
--- src/pages/FilmDetail.vue
+++ src/pages/FilmDetail.vue
@@ -4,6 +4,7 @@ import { useRoute } from 'vue-router'
 import { store } from '../store'
 import { ActionTypes } from '../store/types.d'
 import RoundedChip from '../components/RoundedChip.vue'
+import BackToFilmsButton from '../components/BackToFilmsButton.vue'
 
 const route = useRoute()
 const film = computed(() => store.getters.filmById(route.params.id as string))
@@ -15,27 +16,30 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div class="flex flex-col md:flex-row items-center">
-    <div class="w-full p-8 ml-4 md:ml-16 z-20 pointer-events-none">
-      <h1 v-if="film" class="text-3xl my-4 whitespace-nowrap">{{ film.title }}</h1>
-      <p class="text-6xl my-4 whitespace-nowrap inline mr-4">{{ film?.original_title }}</p>
-      <span v-if="film?.original_title_romanised" class="whitespace-nowrap italic text-sm opacity-40">[ {{ film?.original_title_romanised }} ]</span>
-      <p class="text-m my-4">{{ film?.description }}</p>
-      <div class="opacity-40">
-        <p v-if="film?.director">Director: {{ film?.director }}</p>
-        <p v-if="film?.producer">Producer: {{ film?.producer }}</p>
-        <p v-if="film?.release_date">Released in {{ film?.release_date }}</p>
-        <p v-if="film?.running_time">{{ film?.running_time }} Minutes</p>
+  <div>
+    <BackToFilmsButton />
+    <div class="flex flex-col md:flex-row items-center">
+      <div class="w-full p-8 ml-4 md:ml-16 z-20 pointer-events-none">
+        <h1 v-if="film" class="text-3xl my-4 whitespace-nowrap">{{ film.title }}</h1>
+        <p class="text-6xl my-4 whitespace-nowrap inline mr-4">{{ film?.original_title }}</p>
+        <span v-if="film?.original_title_romanised" class="whitespace-nowrap italic text-sm opacity-40">[ {{ film?.original_title_romanised }} ]</span>
+        <p class="text-m my-4">{{ film?.description }}</p>
+        <div class="opacity-40">
+          <p v-if="film?.director">Director: {{ film?.director }}</p>
+          <p v-if="film?.producer">Producer: {{ film?.producer }}</p>
+          <p v-if="film?.release_date">Released in {{ film?.release_date }}</p>
+          <p v-if="film?.running_time">{{ film?.running_time }} Minutes</p>
+        </div>
+        <div v-if="characters.length" class="py-4">
+            <p>Characters:</p>
+            <div class="flex flex-row flex-wrap mt-4 w-2/3">
+                <RoundedChip v-for="(character, index) in characters" :key="index" :text="character.name" />
+            </div>
+        </div>
       </div>
-      <div v-if="characters.length" class="py-4">
-          <p>Characters:</p>
-          <div class="flex flex-row flex-wrap mt-4 w-2/3">
-              <RoundedChip v-for="(character, index) in characters" :key="index" :text="character.name" />
-          </div>
+      <div class="w-full flex items-center justify-center mt-24">
+        <img class="min-w-min max-h-[80vh]" :src="film?.image" />
       </div>
-    </div>
-    <div class="w-full flex items-center justify-center mt-24">
-      <img class="min-w-min max-h-[80vh]" :src="film?.image" />
     </div>
   </div>
 </template>
