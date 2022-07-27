7dd4bbf58452405d9e775f0d064de96490a7ea1b
Narayan <narayanohanlon@gmail.com>
2022-07-27 22:41:24 +0900

    feat(chip): make generic component for chips
    optional hover behaviour is already built in

---
+++ src/components/RoundedChip.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text: string
+  clickable?: boolean
+}>()
+</script>
+
+<template>
+  <span class="p-2 px-4 m-1 max-w-min bg-white bg-opacity-10 rounded-full whitespace-nowrap transition-all duration-300" :class="{ 'hover:bg-opacity-20' : clickable }">{{ props.text }}</span>
+</template>
--- src/pages/FilmsList.vue
+++ src/pages/FilmsList.vue
@@ -3,6 +3,7 @@ import { computed } from 'vue'
 import { store } from '../store'
 import FilmCard from '../components/FilmCard.vue'
 import FilmSearchBar from '../components/FilmSearchBar.vue'
+import RoundedChip from '../components/RoundedChip.vue'
 import { MutationTypes } from '../store/types.d'
 
 const films = computed(() => store.getters.tenFilms)
@@ -21,11 +22,9 @@ const scrollToStart = () => {
       <div id="grid-container" class="grid grid-flow-row md:grid-flow-col auto-cols-max pl-48 overflow-x-scroll overflow-y-scroll md:overflow-y-clip mt-16 nearly-screen scroll-smooth">
         <div class="inline w-24"></div>
         <FilmCard v-for="(film, index) in films" :key="index" :film="film"/>
-        <div class="m-4 max-w-xs scale-100 cursor-pointer" data-test="ShowMore" @click="showMoreFilms(); scrollToStart()">
+        <div class="m-4 max-w-xs scale-100 cursor-pointer" @click="showMoreFilms(); scrollToStart()">
           <p class="whitespace-nowrap relative top-1/3 -left-1/3 rotate-90">
-            <span class="p-2 px-4 m-1 max-w-min bg-white bg-opacity-10 rounded-full whitespace-nowrap">
-              {{ canShowMoreFilms() ? 'Show More' : 'You\'ve reached the end. Go back.' }}
-            </span>
+            <RoundedChip :text="`${canShowMoreFilms() ? 'Show More' : 'You\'ve reached the end. Go back ⬇'}`" clickable data-test="ShowMore"/>
           </p>
         </div>
         <div class="inline w-24"></div>
