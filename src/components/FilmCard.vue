c7daa4676ca358b56dfda0a36c8cf0b6809d9070
Narayan <narayanohanlon@gmail.com>
2022-07-27 23:52:50 +0900

    feat(style): change background on thumb hover

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Film } from '../store/types'
+import { store } from '../store'
+import { Film, MutationTypes } from '../store/types.d'
 const props = defineProps<{ film: Film }>()
 
 const imageLoading = ref(true)
+
+const setDynamicBackground = (imageUrl: string) => {
+  store.commit(MutationTypes.SET_DYNAMIC_BACKGROUND_URL, imageUrl)
+}
 </script>
 
 <template>
   <div class="inline m-4 max-w-xs" data-test="FilmCard">
-    <RouterLink :to="`/${props.film.id}`">
+    <RouterLink :to="`/${props.film.id}`" @mouseover="setDynamicBackground(props.film.image)">
       <img :src="imageLoading ? './empty.png' : props.film.image" :alt="props.film.title" @load="imageLoading = false" :data-test="`ClickableThumbnail-${film.id}`">
     </RouterLink>
     <div class="mt-2">
