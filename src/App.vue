1f86193456e859d319b7b7a043fee06b75cd0af3
Narayan <narayanohanlon@gmail.com>
2022-07-28 00:05:45 +0900

    feat(style): fade in for a smoother loading feel

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,22 @@ import DynamicBackground from './components/DynamicBackground.vue'
 </script>
 
 <template>
-  <div class="font-mono">
-    <DynamicBackground />
-    <RouterView />
-  </div>
+  <Transition name="fade" mode="out-in" appear>
+    <div class="font-mono">
+      <DynamicBackground />
+      <RouterView />
+    </div>
+  </Transition>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s ease-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
