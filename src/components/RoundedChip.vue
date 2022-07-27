7c83de6f88a9b22e16471e18946276dda497cd4d
Narayan <narayanohanlon@gmail.com>
2022-07-27 23:08:25 +0900

    chore(lint): improve spacing and line breaks

--- src/components/RoundedChip.vue
+++ src/components/RoundedChip.vue
@@ -6,5 +6,10 @@ const props = defineProps<{
 </script>
 
 <template>
-  <span class="p-2 px-4 m-1 max-w-min bg-white bg-opacity-10 rounded-full whitespace-nowrap transition-all duration-300" :class="{ 'hover:bg-opacity-20' : clickable }">{{ props.text }}</span>
+  <span 
+    class="p-2 px-4 m-1 max-w-min bg-white bg-opacity-10 rounded-full whitespace-nowrap transition-all duration-300"
+    :class="{ 'hover:bg-opacity-20' : clickable }"
+  >
+    {{ props.text }}
+  </span>
 </template>
