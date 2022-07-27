33ee9050c3334e5e9a02a9eb797367830818e98b
Narayan <narayanohanlon@gmail.com>
2022-07-27 23:03:37 +0900

    feat(loading): smooth out reflow on initial load

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { Film } from '../store/types'
 const props = defineProps<{ film: Film }>()
+
+const imageLoading = ref(true)
 </script>
 
 <template>
   <div class="inline m-4 max-w-xs" data-test="FilmCard">
     <RouterLink :to="`/${props.film.id}`">
-      <img :src="props.film.image" :alt="props.film.title" :data-test="`ClickableThumbnail-${film.id}`">
+      <img :src="imageLoading ? './empty.png' : props.film.image" :alt="props.film.title" @load="imageLoading = false" :data-test="`ClickableThumbnail-${film.id}`">
     </RouterLink>
-    <div>
+    <div class="mt-2">
       <p data-test="Title">{{ props.film.title }}</p>
       <p data-test="ReleaseDate">{{ props.film.release_date }}</p>
       <p data-test="RunningTime">{{ props.film.running_time }} minutes</p>
