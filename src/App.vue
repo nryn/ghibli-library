a6f73d3b14d774a68b4dd6f7b9e479dce6d9b044
Narayan <narayanohanlon@gmail.com>
2022-07-28 00:16:22 +0900

    feat(style): slide cards up and down on the screen
    animation for transitions between pages

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,24 @@ import DynamicBackground from './components/DynamicBackground.vue'
   <Transition name="fade" mode="out-in" appear>
     <div class="font-mono">
       <DynamicBackground />
-      <RouterView />
+      <RouterView v-slot="{ Component, route }">
+        <Transition
+          mode="out-in"
+          :enter-from-class="(route.meta.enterFrom as string)"
+          :enter-active-class="(route.meta.enterActive as string)"
+          :leave-to-class="(route.meta.leaveTo as string)"
+          :leave-active-class="(route.meta.leaveActive as string)"
+        >
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
     </div>
   </Transition>
 </template>
 
 <style>
+/* Classes for Fade In Animation */
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 2s ease-out;
@@ -20,5 +32,41 @@ import DynamicBackground from './components/DynamicBackground.vue'
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+
+/* Classes for Slide Animation */
+
+.enter-from-top {
+  transition: all 0.2s ease-out;
+  transform: translateY(-100vh);
+}
+.enter-active-from-top {
+  transition: all 0.2s ease-out;
+}
+
+.leave-active-to-top {
+  transition: all 0.2s ease-in;
+}
+.leave-to-top {
+  transition: all 0.2s ease-in;
+  transform: translateY(100vh);
+}
+
+.enter-from-bottom {
+  transition: all 0.2s ease-out;
+  transform: translateY(100vh);
+}
+
+.enter-active-from-bottom {
+  transition: all 0.2s ease-out;
+}
+
+.leave-active-to-bottom {
+  transition: all 0.2s ease-in;
+}
+
+.leave-to-bottom {
+  transition: all 0.2s ease-in;
+  transform: translateY(-100vh);
 }
 </style>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,9 +5,21 @@ export const routes = [
   {
     path: '/',
     component: FilmsList,
+    meta: {
+      enterFrom: "enter-from-top",
+      leaveTo: "leave-to-top",
+      enterActive: "enter-active-from-top",
+      leaveActive: "leave-active-to-top",
+    }
   },
   {
     path: '/:id',
     component: FilmDetail,
+    meta: {
+      enterFrom: "enter-from-bottom",
+      leaveTo: "leave-to-bottom",
+      enterActive: "enter-active-from-bottom",
+      leaveActive: "leave-active-to-bottom",
+    }
   },
 ]
