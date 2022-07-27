22a84afdec822539c5cf65b3d9564ca4019f90e0
Narayan <narayanohanlon@gmail.com>
2022-07-28 00:02:40 +0900

    refactor(style): move dynamic bg to component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { store } from './store'
-
-  const dynamicBackgroundUrl = computed(() => store.getters.dynamicBackgroundUrl)
+import DynamicBackground from './components/DynamicBackground.vue'
 </script>
 
 <template>
   <div class="font-mono">
-    <div
-    id="bg"
-    class="absolute inset-0 h-full bg-center bg-no-repeat bg-cover -z-10 blur-[120px] opacity-50"
-    :style="{ backgroundImage: 'url(' + dynamicBackgroundUrl + ')' }"
-  ></div>
+    <DynamicBackground />
     <RouterView />
   </div>
 </template>
-
-<style>
-#bg {
-  transition: background-image 0.3s ease-in-out;
-}
-</style>
---
+++ src/components/DynamicBackground.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { store } from '../store'
+
+const dynamicBackground = computed(() => store.getters.dynamicBackgroundUrl)
+</script>
+
+<template>
+  <div
+    id="bg"
+    class="absolute inset-0 h-full bg-center bg-no-repeat bg-cover -z-10 blur-[120px] opacity-50"
+    :style="{ backgroundImage: 'url(' + dynamicBackground + ')' }"
+  ></div>
+</template>
+
+<style>
+#bg {
+    transition: background-image 0.3s ease-in-out;
+}
+</style>
