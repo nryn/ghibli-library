4b9a25e29d60caaf33f7ea3443e5b1507e88452c
Narayan <narayanohanlon@gmail.com>
2022-07-27 23:10:48 +0900

    feat(style): add some hover behaviour to top link

--- src/components/BackToFilmsButton.vue
+++ src/components/BackToFilmsButton.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="fixed top-8 flex px-8 pb-8 w-full justify-center text-xl">
-    <RouterLink to="/"><p>⬆<span class="opacity-40 drop-shadow-2xl"> All Films</span></p></RouterLink>
+    <RouterLink to="/"><p>⬆<span class="opacity-40 drop-shadow-2xl hover:opacity-80 transition-opacity duration-500"> All Films</span></p></RouterLink>
   </div>
 </template>
