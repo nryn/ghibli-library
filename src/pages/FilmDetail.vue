93cde686299ade21651b5ec113aaab576c38c9f1
Narayan <narayanohanlon@gmail.com>
2022-07-27 22:25:40 +0900

    feat(style): improve look and feel of film details

--- src/pages/FilmDetail.vue
+++ src/pages/FilmDetail.vue
@@ -14,21 +14,24 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div>
-    <p>{{ film?.title }}</p>
-    <p>{{ film?.original_title }}</p>
-    <p>{{ film?.original_title_romanised }}</p>
-    <p>{{ film?.description }}</p>
-    <p>{{ film?.title }}</p>
-    <p>Director: {{ film?.director }}</p>
-    <p>Producer: {{ film?.producer }}</p>
-    <p>Released in {{ film?.release_date }}</p>
-    <p>{{ film?.running_time }} Minutes</p>
-    <div v-if="film?.people?.length">
-        <p>Characters:</p>
-        <div>
-            <p v-for="(character, index) in characters" :key="index">{{ character.name }}</p>
-        </div>
+  <div class="flex flex-col md:flex-row items-center">
+    <div class="w-full p-8 ml-4 md:ml-16 z-20 pointer-events-none">
+      <h1 v-if="film" class="text-3xl my-4 whitespace-nowrap">{{ film.title }}</h1>
+      <p class="text-6xl my-4 whitespace-nowrap inline mr-4">{{ film?.original_title }}</p>
+      <span v-if="film?.original_title_romanised" class="whitespace-nowrap italic text-sm opacity-40">[ {{ film?.original_title_romanised }} ]</span>
+      <p class="text-m my-4">{{ film?.description }}</p>
+      <div class="opacity-40">
+        <p v-if="film?.director">Director: {{ film?.director }}</p>
+        <p v-if="film?.producer">Producer: {{ film?.producer }}</p>
+        <p v-if="film?.release_date">Released in {{ film?.release_date }}</p>
+        <p v-if="film?.running_time">{{ film?.running_time }} Minutes</p>
+      </div>
+      <div v-if="film?.people?.length" class="py-4">
+          <p>Characters:</p>
+          <div>
+              <p v-for="(character, index) in characters" :key="index">{{ character.name }}</p>
+          </div>
+      </div>
     </div>
     <img :src="film?.image" />
   </div>
